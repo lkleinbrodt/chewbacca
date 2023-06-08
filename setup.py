df23a37b266861798fb73ccb4ecdb4e1870a98a7
Landon Kleinbrodt <lkleinbrodt@gmail.com>
2023-06-08 12:19:38 -0700

    build a package!

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
+from setuptools import setup
+if __name__ == '__main__':
+    setup()
 
-setup(
-    name='chewbacca',
-    version='0.3',
-    author='Landon Kleinbrodt',
-    author_email='lkleinbrodt@gmail.com',
-    packages=find_packages(),
-    install_requires = [
-        'openai',
-        'python-dotenv'
-    ],
-    entry_points={
-        'console_scripts': [
-            'chewbacca-document=src.cli:document',
-        ],
-    },
-)
+# setup(
+#     name='chewbacca',
+#     version='0.3',
+#     author='Landon Kleinbrodt',
+#     author_email='lkleinbrodt@gmail.com',
+#     packages=find_packages(),
+#     install_requires = [
+#         'openai',
+#         'python-dotenv'
+#     ],
+#     entry_points={
+#         'console_scripts': [
+#             'chewbacca-document=src.cli:document',
+#         ],
+#     },
+# )
