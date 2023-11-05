82d8118359056c798638d3b5ff54db5caa4504ee
Landon Kleinbrodt <lkleinbrodt@gmail.com>
2023-11-05 12:25:24 -0800

    update

--- chewbacca/app.py
+++ chewbacca/app.py
@@ -1,12 +1,16 @@
 import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_sdk.signature import SignatureVerifier
+from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, request
+from flask import Flask, request, abort
 from functions import respond
+import logging
+from functools import wraps
+import time
+import sys
 
 # Load environment variables from .env file
 load_dotenv(find_dotenv())
